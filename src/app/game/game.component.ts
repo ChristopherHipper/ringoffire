693242008637c6b3c932f55f2cf49d7b7249346b
Christopher Hipper <hipper.christopher@gmail.com>
2025-12-22 23:19:19 +0100

    cointinue game design an starting game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent{
+export class GameComponent implements OnInit {
+  game!: Game;
+  currentCard: string | undefined = '';
+  pickCardAnimation: boolean = false;
+  rotation = '0deg';
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  };
+
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.rotation = this.getRandomRotation();
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        if (this.currentCard != undefined) {
+          this.game.playedCards.push({
+            name : this.currentCard,
+            rotation: this.rotation
+          })
+        } else {
+          alert('Game End')
+        }
+      }, 1000);
+    }
+  };
+
+  getRandomRotation(): string {
+    return `${Math.floor(Math.random() * 5) - 2}deg`;
+  }
+
 
 }
---
+++ src/app/models/game.ts
@@ -0,0 +1,24 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: { name: string; rotation: string }[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        this.createStack()
+    };
+
+    createStack() {
+        for (let index = 1; index < 14; index++) {
+            this.stack.push('clubs_' + index);
+            this.stack.push('diamonds_' + index);
+            this.stack.push('hearts_' + index);
+            this.stack.push('spade_' + index);
+        };
+        this.shuffleStack(this.stack);
+    }
+
+    shuffleStack(arr: String[]) {
+        arr.sort(() => Math.random() - 0.5);
+    };
+};
